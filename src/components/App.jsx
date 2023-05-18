d88a83ecaaf2ad789c11c514c9d3d31ee8541777
Nepalcem <mark.emailtest@gmail.com>
2023-05-18 20:59:49 +0300

    init #3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
-export const App = () => {
-  return <div>123</div>;
-};
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>PhoneBook</h1>
+
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+    );
+  }
+}
