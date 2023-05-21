2aedb1eaae63b5ad4f6f09f52141cff596b5f340
Nepalcem <mark.emailtest@gmail.com>
2023-05-21 18:04:53 +0300

    proptypes + styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FilterStyled } from './Filter.styled';
 
-const Filter = ({value, onChange}) => {
+const Filter = ({ value, onChange }) => {
   return (
+    <FilterStyled>
     <label>
-          Filter by name:
-          <input type="text" value={value} onChange={onChange}/>
-        </label>
-  )
-}
+      Filter by name:
+      <input className="filter-input" type="text" value={value} onChange={onChange} />
+    </label>
+    </FilterStyled>
+  );
+};
 
-export default Filter
+export default Filter;
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
--- src/components/contactsList/ContactsList.jsx
+++ src/components/contactsList/ContactsList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { ContactsStyled } from './ContactsList.styled';
 
 const ContactsList = ({ contacts, removeContact }) => {
   return (
     <div className="contacts">
       <h2>Contacts List:</h2>
-      <ul>
+      <ContactsStyled>
         {contacts.map(({ name, id, number }) => (
-          <li key={id}>
-            <span>{name}</span>: <span>{number} </span>
-            <button onClick={() => removeContact(id)}>X</button>
+          <li key={id} className="item">
+            <span className="name">{name}</span>: <span>{number} </span>
+            <button onClick={() => removeContact(id)}>Delete</button>
           </li>
         ))}
-      </ul>
+      </ContactsStyled>
     </div>
   );
 };
 
 export default ContactsList;
+
+ContactsList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func.isRequired,
+};
